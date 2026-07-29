f8ed3a49e48e7afb512f25c097742bb35f47381c
brbuh <309535950+brbuh@users.noreply.github.com>
2026-07-29 11:22:07 +0300

    English translate

--- Morze_Beeper_RU.py
+++ Morze_Beeper_RU.py
@@ -3,7 +3,7 @@ import winsound
 
 print("=== Morse Beeper v1.0 by brbuh ===")
 
-print('Press "-" or "j" to play "—", press "k" or "." to play "·".')
+print('Нажмите "-" или "j" чтобы издать "—", нажмите "k" или "." чтобы издать "·".')
 
 
 wpm_modes_long = {
@@ -19,15 +19,15 @@ wpm_modes_short = {
 }
 
 try:
-    choice = int(input('Select mode: 1 - very slow (5 WPM), 2 - standard (12 WPM), 3 - fast (20 WPM): '))
+    choice = int(input('Выберите режим: 1 - очень медленно(5 WPM), 2 - стандартно(12 WPM), 3 - быстро(20 WPM): '))
 except ValueError:
     choice = None
 
 if choice not in (1, 2, 3):
-    print('It looks like you made a typo. We selected standard mode for you.')
+    print('Похоже, вы ошиблись в вводе. Мы выбрали за вас стандартный режим.')
 
 
-print("To exit, press esc")
+print("Чтобы выйти, нажмите esc")
 
 
 long = wpm_modes_long.get(choice, 300)
